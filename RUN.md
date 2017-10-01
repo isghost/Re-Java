622389ea98c5ce4f08687420ba41ee8b4bf49223
ccy <810278677@qq.com>
2017-10-01 18:21:26 +0800

    [upd] 将工程转换成纯maven工程，不依赖编辑器 [fix] 替换已经被废弃的 com.sun.image.codec.jpeg ，替换成ImageIO

--- RUN.md
+++ RUN.md
@@ -5,8 +5,9 @@
 	1. 下载安装
 	2. 破解 Help --> Register --> License server --> 输入 http://idea.iteblog.com/key.php --> Active
 	3. 安装lombok插件 File --> Settings --> Plugins --> Browse repositories... --> 搜索lombok --> 安装重启
-
-3. jdk 1.8环境 略
-4. 查看配置，web.xml的spring.profiles.default修改成prod，这样直接连接服务器数据库。如果后端开发修改数据库结构，需要自行配置mysql，执行sql文件。
-5. 安装tomcat，并在IDEA中配置
-6. 打开工程，选择用tomcat运行
+	4. eclipse同理安装lombok插件
+3. 在IDE导入已经存在的maven工程(具体步骤百度)
+4. jdk 1.8环境 略
+5. 查看配置，web.xml的spring.profiles.default修改成prod，这样直接连接服务器数据库。如果后端开发修改数据库结构，需要自行配置mysql，执行sql文件。
+6. 安装tomcat，并在IDEA中配置
+7. 打开工程，选择用tomcat运行
--- src/main/java/www/ccyblog/novel/modules/account/web/AccountController.java
+++ src/main/java/www/ccyblog/novel/modules/account/web/AccountController.java
@@ -1,8 +1,6 @@
 package www.ccyblog.novel.modules.account.web;
 
 import com.octo.captcha.service.CaptchaServiceException;
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
 import lombok.extern.log4j.Log4j;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
@@ -11,13 +9,16 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import www.ccyblog.novel.common.security.UserAuthenticationToken;
 import www.ccyblog.novel.modules.account.service.AccountService;
 import www.ccyblog.novel.modules.account.service.AccountService.REGISTER_ERROR_INFO;
 import www.ccyblog.novel.modules.account.service.JCaptchaService;
 
+import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -102,12 +103,10 @@ public class AccountController {
             BufferedImage challenge =
                     jCaptchaService.getImageChallengeForID(captchaId,
                             httpServletRequest.getLocale());
+            ByteArrayOutputStream byteArrayInputStream = new ByteArrayOutputStream();
 
-            // jpeg编码器
-
-            JPEGImageEncoder jpegEncoder =
-                    JPEGCodec.createJPEGEncoder(jpegOutputStream);
-            jpegEncoder.encode(challenge);
+            ImageIO.write(challenge, "jpeg", byteArrayInputStream);
+            captchaChallengeAsJpeg = byteArrayInputStream.toByteArray();
 
         } catch (IllegalArgumentException e) {
             try {
@@ -126,8 +125,6 @@ public class AccountController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
 
         // 写入响应
         httpServletResponse.setHeader("Cache-Control", "no-store");
